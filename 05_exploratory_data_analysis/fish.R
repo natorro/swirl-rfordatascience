abe9d7f8d382b643a4da6875aa2cb66af53fe44c
Ricardo Ruiz <haloplatinum@hotmail.es>
2018-11-12 17:12:37 -0600

    I added file fish.csv

--- 05_exploratory_data_analysis/fish.R
+++ 05_exploratory_data_analysis/fish.R
@@ -1,6 +1,6 @@
-peces = read.csv(file ='Documents/fish.csv',header = TRUE)
+peces <- read.csv('/home/ricardo/swirl-rfordatascience/05_exploratory_data_analysis/fish.csv', header = TRUE)
 print(peces)
 str(peces)
 head(peces)
-barplot(prop.table(table(peces$Especies)), main = "Especies de peces",xlab = "Especies",ylab = "Contéo",width = 1)
+barplot(prop.table(table(peces$Especies)), main = "Especies de peces", xlab = "Especies", ylab = "Contéo", width = 1)
 
