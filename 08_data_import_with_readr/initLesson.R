397de6134ef922c37503e38f40f78a68a69c761c
flynngodslayer <victor_avila_felipe@hotmail.com>
2018-11-13 19:56:55 -0600

    Added csv object to learn the read_csv function

--- 08_data_import_with_readr/initLesson.R
+++ 08_data_import_with_readr/initLesson.R
@@ -2,3 +2,10 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
+.obtener_direccion <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+direccion <- file.path(.obtener_direccion(), "swirl-rfordatascience", "08_data_import_with_readr")
